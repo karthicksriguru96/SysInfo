278512591e5384b873e66617f33adceec618e456
karthicksriguru96 <49912625+karthicksriguru96@users.noreply.github.com>
2020-09-21 08:44:02 -0700

    Update SysInfo.ps1

--- SysInfo/SysInfo.ps1
+++ SysInfo/SysInfo.ps1
@@ -1,1 +1,1 @@
-﻿Get-ChildItem -Path $PSScriptRoot\Public\*.ps1,$PSScriptRoot\Private\*.ps1 | ForEach-Object {. $_.FullName}
+﻿Get-ChildItem -Path $PSScriptRoot\Public\*.ps1,$PSScriptRoot\Private\*.ps1 | ForEach-Object {. $_.Name}
